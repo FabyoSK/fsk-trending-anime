7eba28b3ef2746198e839d15acd6c349ffc4729f
Fábio Alves <fabioalves.contacto@gmail.com>
2021-04-12 15:07:14 -0100

    Feat: add card style and click to read more

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -14,6 +14,8 @@ interface AnimeInfo {
 }
 export function Landing() {
   const [aniTrend, setAniTrend] = useState<AnimeInfo[]>([]);
+  const [readMore, setReadMore] = useState(false);
+  const MAX_LENGTH = 250;
 
   useEffect(() => {
     api.get("trending/anime").then((response) => {
@@ -22,19 +24,34 @@ export function Landing() {
     });
   }, []);
 
+  function handleShowMore(content: string) {
+    setReadMore(!readMore);
+  }
   return (
-    <div>
+    <div className="anime-card-section">
       {aniTrend.map((anime: AnimeInfo) => {
         return (
           <div key={anime.id} className="anime-card">
-            <h1>{anime.id}</h1>
+            {/* <h1>{anime.id}</h1> */}
             <img
               src={anime.attributes.posterImage.large}
               alt=""
               className="anime-card-img"
             />
-            <h2>{anime.attributes.canonicalTitle}</h2>
-            <p>{anime.attributes.description}</p>
+            <div className="anime-card-info">
+              <h2>{anime.attributes.canonicalTitle}</h2>
+
+              {!readMore
+                ? anime.attributes.description.substring(0, MAX_LENGTH)
+                : anime.attributes.description}
+              <p></p>
+              <button
+                className="anime-card-button"
+                onClick={() => handleShowMore(anime.attributes.description)}
+              >
+                Clicame
+              </button>
+            </div>
           </div>
         );
       })}
