b9229b7c95178d5be36c011fa44b1e667d641d91
Fábio Alves <fabioalves.contacto@gmail.com>
2021-04-16 20:17:16 -0100

    Chore: add search route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,13 +3,16 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Landing } from "./pages/Landing";
 import { TrendingAnimes } from "./pages/Trending";
 import { AnimeInfo } from "./pages/AnimeInfo";
+import { AnimeSearch } from "./pages/AnimeSearch";
+
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Landing} />
-        <Route path="/trending/animes" exact component={TrendingAnimes} />
+        <Route path="/trending/animes" component={TrendingAnimes} />
         <Route path="/anime/:id" component={AnimeInfo} />
+        <Route path="/search/anime" component={AnimeSearch} />
       </Switch>
     </BrowserRouter>
   );
